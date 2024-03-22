80a235b02cf6f7e9b1caa7958049258ee7375d48
Beatriz <bdecastroresende@gmail.com>
2024-03-22 15:27:57 -0300

    Correção frontend-teste1, pasta subiu sem arquivos

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <button className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-md px-10 py-2.5 text-center me-2 mb-2">Botão</button>
+  );
+}
+
+export default App;
